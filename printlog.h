d886563cdf4295b869f43b25fb1bcb903bbc1b14
JackZhao <kobe2014zyp@126.com>
2018-08-14 15:47:10 +0800

    Encrypt example

--- printlog.h
+++ printlog.h
@@ -6,15 +6,15 @@
 // #define debug_msg(fmt, arg...) do { } while (0)       
 // #define init_msg(fmt, arg...)  do { } while (0)
 // #define err_msg(fmt, arg...)   do { } while (0)
-// #define info_msg(fmt, arg...)  do { } while (0)
-// #define warn_msg(fmt, arg...)  do { } while (0)
+#define info_msg(fmt, arg...)  do { } while (0)
+#define warn_msg(fmt, arg...)  do { } while (0)
 
 
-#define debug_msg(fmt, arg...) do { printf("[DEBUG]\t" fmt, ## arg)} while (0)       
+#define debug_msg(fmt, arg...) do { printf("[DEBUG]\t" fmt, ## arg);} while (0)       
 #define init_msg(fmt, arg...)  do { printf("[INIT ]\t" fmt, ## arg);} while (0)
 #define err_msg(fmt, arg...)   do { printf("[ERR  ]\t" fmt, ## arg);} while (0)
-#define info_msg(fmt, arg...)  do { printf("[INFO ]\t" fmt, ## arg);} while (0)
-#define warn_msg(fmt, arg...)  do { printf("[WARN ]\t" fmt, ## arg); } while (0)
+// #define info_msg(fmt, arg...)  do { printf("[INFO ]\t" fmt, ## arg);} while (0)
+// #define warn_msg(fmt, arg...)  do { printf("[WARN ]\t" fmt, ## arg); } while (0)
 
 #endif
 
