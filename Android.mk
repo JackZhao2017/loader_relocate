253c5f99f34267b7f1ef833984a335be61f6bb65
JackZhao <kobe2014zyp@126.com>
2018-08-14 15:46:03 +0800

    Encrypt example

--- Android.mk
+++ Android.mk
@@ -5,49 +5,34 @@ LOCAL_PATH := $(root_path)
 include $(CLEAR_VARS)
 
 
-LOCAL_MODULE :=loader.so
+LOCAL_MODULE :=firstshared.so
 
-LOCAL_MODULE_FILENAME:=libloader
+LOCAL_MODULE_FILENAME:=libfirstshared
 
 LOCAL_C_INCLUDES:=$(LOCAL_PATH)/
 
-LOCAL_SRC_FILES := $(LOCAL_PATH)/loader.cpp
-LOCAL_SRC_FILES += $(LOCAL_PATH)/linker_allocator.cpp
+
+
+LOCAL_SRC_FILES := $(LOCAL_PATH)/linker_allocator.cpp
 LOCAL_SRC_FILES += $(LOCAL_PATH)/loader_soinfo.cpp
 LOCAL_SRC_FILES += $(LOCAL_PATH)/loader_phdr.cpp
-
+LOCAL_SRC_FILES += $(LOCAL_PATH)/loader_addr.cpp
+LOCAL_SRC_FILES += $(LOCAL_PATH)/loader.cpp
 
 include $(BUILD_SHARED_LIBRARY)
 
 
 
-
-
-
-
-
 # include $(CLEAR_VARS)
 
-# LOCAL_MODULE    :=pre-load
-# LOCAL_SRC_FILES :=$(LOCAL_PATH)/libs/armeabi-v7a/libloader.so
 
-# include $(PREBUILT_SHARED_LIBRARY)
+# LOCAL_MODULE :=addr.out
 
+# LOCAL_C_INCLUDES:=$(LOCAL_PATH)/
 
-
-
-# include $(CLEAR_VARS)
-
-# LOCAL_MODULE := main.out
-
-# LOCAL_SRC_FILES := $(LOCAL_PATH)/main.cpp
-
-# LOCAL_C_INCLUDES :=$(LOCAL_PATH)/
-# LOCAL_SHARED_LIBRARIES := pre-load
-
+# LOCAL_SRC_FILES := $(LOCAL_PATH)/loader_addr.cpp
 
 # include $(BUILD_EXECUTABLE)
-
 
 
 
